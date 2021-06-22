e601dd770d7c53153721c24025c55629070debfc
jaimeraynaud <jaimeraynauds@gmail.com>
2021-06-22 17:03:47 +0200

    Version terminada sin búsqueda de hiperparámetros

--- workspace-project/exploracion_bikes.py
+++ workspace-project/exploracion_bikes.py
@@ -14,94 +14,87 @@ import statsmodels.formula.api as smf
 import seaborn as sns
 from scipy import stats
 
-df = pd.read_csv("../data/train.csv")
 
-print('Cantidad de Filas y columnas:',df.shape)
-print('Nombre de las features:\n',df.columns)
+data = pd.read_csv("../data/train.csv")
+
+print('Cantidad de Filas y columnas:',data.shape)
+print('Nombre de las features:\n',data.columns)
 
 '''Para comenzar a analizar los datos hacemos print de las variables y las primeras 5 filas de datos'''
-print(df.head(5))
+print(data.head(5))
 
 #Tipo de los distintos datos que aparecen:
-print(df.info()) #Podemos observar que no hay valores nulos
+print(data.info()) #Podemos observar que no hay valores nulos
 
 #Pandas filtra las features numericas y calcula datos estadísticos que pueden ser útiles: 
 #cantidad, media, desvío estandar, valores máximo y mínimo
-print(df.describe())
+print(data.describe())
 
 
 #Antes de mostrar gráficas vamos a adaptar las features para un mejor entendimiento, usando Feature Engineering
 
-df["date"] = df.datetime.apply(lambda x : x.split()[0])
-df["hour"] = df.datetime.apply(lambda x : x.split()[1].split(":")[0])
+data['hour'] = data.loc[:,'datetime'].dt.hour
+data['day'] = data.loc[:,'datetime'].dt.weekday
+data['month'] = data.loc[:,'datetime'].dt.month
+data['year'] = data.loc[:,'datetime'].dt.year
 
-df["weekday"] = df.date.apply(lambda dateString : calendar.day_name[datetime.strptime(dateString,"%Y-%m-%d").weekday()])
-df["month"] = df.date.apply(lambda dateString : calendar.month_name[datetime.strptime(dateString,"%Y-%m-%d").month])
-df["season"] = df.season.map({1: "Spring", 2 : "Summer", 3 : "Fall", 4 :"Winter" })
-df["weather"] = df.weather.map({1: "Good",\
+#Eliminamos la columna datetime ya que ya no nos es necesaria
+data = data.drop(["datetime"],axis=1)
+
+data["season"] = data.season.map({1: "Spring", 2 : "Summer", 3 : "Fall", 4 :"Winter" })
+data["weather"] = data.weather.map({1: "Good",\
                                               2 : "Cloudy", \
                                               3 : "Bad", \
                                               4 :"Very bad" })
-    
-categoryVariableList = ["hour","weekday","month","season","weather","holiday","workingday"]
-for var in categoryVariableList:
-    df[var] = df[var].astype("category")
 
-#Eliminamos la columna datetime ya que ya no nos es necesaria
-df = df.drop(["datetime"],axis=1)
+#Pasamos a tipo category las categorical features
+data[['season','holiday','workingday','weather', 'year','month','day','hour']] = data[['season','holiday','workingday','weather', 'year','month','day','hour']].astype('category')
+
+
 
 #Ahora veamos las boxplot de count:
-
-sns.boxplot(data=df,y="count",orient="v")
+sns.boxplot(data=data,y="count",orient="v")
 
 #Veamos si hay correlacion entre las variables:
-corr = df.set_index('count').corr()
+corr = data.set_index('count').corr()
 sm.graphics.plot_corr(corr, xnames=list(corr.columns))
 plt.show()
+
 #Al ver que atemp y temp están altamente correlacionadas eliminaremos una de ellas.
-df = df.drop('atemp',axis=1) 
+data = data.drop('atemp',axis=1) 
 
 #Outliers
-Q1 = df["count"].quantile(0.25)
-Q3 = df["count"].quantile(0.75)
+Q1 = data["count"].quantile(0.25)
+Q3 = data["count"].quantile(0.75)
 IQR = Q3 - Q1
 
-print("Shape con outliers: ", df.shape) 
+print("Shape con outliers: ", data.shape) 
   
 # Upper bound
-upper = np.where(df['count'] >= (Q3+1.5*IQR))
+upper = np.where(data['count'] >= (Q3+1.5*IQR))
 # Lower bound
-lower = np.where(df['count'] <= (Q1-1.5*IQR))
+lower = np.where(data['count'] <= (Q1-1.5*IQR))
   
 ''' Eliminamos los outliers '''
-df.drop(upper[0], inplace = True)
-df.drop(lower[0], inplace = True)
+data.drop(upper[0], inplace = True)
+data.drop(lower[0], inplace = True)
   
-print("Shape sin outliers: ", df.shape)
+print("Shape sin outliers: ", data.shape)
 
-#Veamos ahora la grafica de distribución de densidad de la variable count
-fig,axes = plt.subplots(ncols=2,nrows=2)
-fig.set_size_inches(12, 10)
-sns.distplot(df["count"],ax=axes[0][0])
-stats.probplot(df["count"], dist='norm', fit=True, plot=axes[0][1])
-
-#Al aplicarle la funcion logaritmo nos queda mucho más parecida a una distribución normal
-sns.distplot(np.log(df["count"]),ax=axes[1][0])
-stats.probplot(np.log1p(df["count"]), dist='norm', fit=True, plot=axes[1][1])
-
-sns.factorplot(x='season',data=df,kind='count',size=5,aspect=2)
-sns.factorplot(x='holiday',data=df,kind='count',size=5,aspect=2)
-sns.factorplot(x='workingday',data=df,kind='count',size=5,aspect=2)
-sns.factorplot(x='weather',data=df,kind='count',size=5,aspect=2) 
+'''Veamos varios diagramas de barras para cada una de las features de nuestro conjunto'''
+sns.factorplot(x='season',data=data,kind='count',size=5,aspect=2)
+sns.factorplot(x='holiday',data=data,kind='count',size=5,aspect=2)
+sns.factorplot(x='workingday',data=data,kind='count',size=5,aspect=2)
+sns.factorplot(x='weather',data=data,kind='count',size=5,aspect=2) 
 
 fig,axes=plt.subplots(2,2)
-axes[0,0].hist(x="temp",data=df,edgecolor="black",linewidth=2)
+axes[0,0].hist(x="temp",data=data,edgecolor="black",linewidth=2)
 axes[0,0].set_title("Temp")
-axes[0,1].hist(x="atemp",data=df,edgecolor="black",linewidth=2)
+axes[0,1].hist(x="atemp",data=data,edgecolor="black",linewidth=2)
 axes[0,1].set_title("Atemp")
-axes[1,0].hist(x="windspeed",data=df,edgecolor="black",linewidth=2)
+axes[1,0].hist(x="windspeed",data=data,edgecolor="black",linewidth=2)
 axes[1,0].set_title("Windspeed")
-axes[1,1].hist(x="humidity",data=df,edgecolor="black",linewidth=2)
+axes[1,1].hist(x="humidity",data=data,edgecolor="black",linewidth=2)
 axes[1,1].set_title("Humidity")
 fig.set_size_inches(10,10)
 
--- workspace-project/prediccion_bikes.py
+++ workspace-project/prediccion_bikes.py
@@ -9,25 +9,24 @@ Created on Fri Jun 18 19:04:04 2021
  
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_squared_log_error
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-import matplotlib.pyplot as plt
 import lightgbm as lgb
 from sklearn.metrics import r2_score
+
 
 def read_files():
     train = pd.read_csv("../data/train.csv", parse_dates = ["datetime"])
     test = pd.read_csv("../data/test.csv", parse_dates = ["datetime"])
     
     train['hour'] = train.loc[:,'datetime'].dt.hour
-    train['day'] = train.loc[:,'datetime'].dt.day
+    train['day'] = train.loc[:,'datetime'].dt.weekday
     train['month'] = train.loc[:,'datetime'].dt.month
     train['year'] = train.loc[:,'datetime'].dt.year
     
     test['hour'] = test.loc[:,'datetime'].dt.hour
-    test['day'] = test.loc[:,'datetime'].dt.day
+    test['day'] = test.loc[:,'datetime'].dt.weekday
     test['month'] = test.loc[:,'datetime'].dt.month
     test['year'] = test.loc[:,'datetime'].dt.year
     
@@ -52,61 +51,63 @@ def feature_engineering(train, test):
     ''' Eliminamos los outliers '''
     train.drop(upper[0], inplace = True)
     train.drop(lower[0], inplace = True)
-    
-# =============================================================================
-#     train['windspeed'] = train['windspeed'].replace(0,np.NaN)
-#     test['windspeed'] = test['windspeed'].replace(0,np.NaN)
-#     train['windspeed'] = train['windspeed'].interpolate()
-#     test['windspeed'] = test['windspeed'].interpolate()
-# =============================================================================
+
     #Convertimos los datos a categorical para poder trabajar con ellos.
     train[['season','holiday','workingday','weather', 'year','month','day','hour']] = train[['season','holiday','workingday','weather', 'year','month','day','hour']].astype('category')
     test[['season','holiday','workingday','weather', 'year','month','day','hour']] = test[['season','holiday','workingday','weather', 'year','month','day','hour']].astype('category')    
     return train, test
 
 train, test = read_files()
-train.info()
-test.info()
 train_sin_outliers, test = feature_engineering(train, test)
-train_sin_outliers.info()
 
-def rmse(y_actual, y_predicted):
-    rmse = mean_squared_error(y_actual, y_predicted, squared=False)
-    return rmse
+def models(train):
+    
+    train = train.drop('casual',axis=1)
+    train = train.drop('registered',axis=1)
+    
+    x = train.drop('count',axis=1)
+    y = train['count']
+    x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.25,random_state=42)
+    
+    '''Random Forest Model'''
+    rf_model = RandomForestRegressor(n_estimators= 100)
+    rf_model.fit(x_train, y_train)
+    y_pred_rf = rf_model.predict(x_test)
+    
+    print("RMSE para Random Forest: ", np.sqrt(mean_squared_error(y_test,y_pred_rf)))
+    print("R2 para Random Forest: ", r2_score(y_test, y_pred_rf))
+    
+    '''Light Gradient Boost Model'''
+    lgb_model = lgb.LGBMRegressor(n_estimators=4000,alpha=0.01)
+    lgb_model.fit(x_train, y_train)
+    y_pred_lgb = lgb_model.predict(x_test)
+    
+    print("RMSE para LGBM: ",np.sqrt(mean_squared_error(y_test,y_pred_lgb)))
+    print("R2 para LGBM: ", r2_score(y_test, y_pred_lgb))
+    
+    '''Gradient Boost Model'''
+    gb_model = GradientBoostingRegressor(n_estimators=4000,alpha=0.01);
+    gb_model.fit(x_train, y_train)
+    y_pred_gb = gb_model.predict(x_test)
 
-def work(train, test):
-    print(train.describe())
-    x_train,x_test,y_train,y_test=train_test_split(train.drop('count',axis=1),train['count'],test_size=0.25,random_state=42)
+    print("RMSE para GBM: ",np.sqrt(mean_squared_error(y_test,y_pred_gb)))
+    print("R2 para GBM: ", r2_score(y_test, y_pred_gb))
 
-    regressor = RandomForestRegressor(n_estimators= 1000, max_depth= 15, random_state=0, min_samples_split= 5, n_jobs=-1)
-    regressor.fit(x_train, y_train)
-    y_pred = regressor.predict(x_test)
     
-    r = rmse(y_test, y_pred)
-    rSquaredRF = r2_score(y_test, y_pred)
-    print("RMSE para Random Forest: %f" % (r))
-    print("R2 para Random Forest: %f" % (rSquaredRF))
+models(train_sin_outliers)
+
+def prediccion(train, test):
+    x = train.drop('count',axis=1)
+    y = train['count']
+    y_log = np.log(y.astype(float))
     
-    hyperparameters = { 'colsample_bytree': 0.725,  'learning_rate': 0.013,
-                    'num_leaves': 56, 'reg_alpha': 0.754, 'reg_lambda': 0.071, 
-                    'subsample': 0.523, 'n_estimators': 1093}
-    model = lgb.LGBMRegressor(**hyperparameters)
-    model.fit(x_train, y_train)
-    predslgbm = model.predict(x_test)
-    
-    r2 = rmse(y_test, predslgbm)
-    rSquaredLGB = r2_score(y_test, predslgbm)
-    print("RMSE para LGBM: %f" % (r2))
-    print("R2 para LGBM: %f" % (rSquaredLGB))
-    
-    gbm = GradientBoostingRegressor(n_estimators=4000,alpha=0.01);
-    gbm.fit(x_train, y_train)
-    predsGBM = gbm.predict(x_test)
-    r3 = rmse(y_test, predsGBM)
-    rSquaredGB = r2_score(y_test, predsGBM)
-    print("RMSE para GBM: %f" % (r3))
-    print("R2 para GBM: %f" % (rSquaredGB))
+    gb_model = GradientBoostingRegressor(n_estimators=4000,alpha=0.01);
+    gb_model.fit(x, y_log)
     
     
+    y_pred_gb = gb_model.predict(np.log(test))
     
-work(train_sin_outliers, test)
+    return np.exp(y_pred_gb)
+
+#preds = prediccion(train_sin_outliers, test)
+#print(preds)
